314ef599c6f3fd77dc20d19704a8910685ea4dc6
Danish Xavier <danishxr@gmail.com>
2019-01-23 16:55:22 +0530

    Updated Script
    * Added directory exception handling.
    * Added Checks to handle to take only image files.
    * Code following most PEP 8 methodology.
    
    Future update.
    ----------------------------
    Better use of algorithms for higher performance.

--- script.py
+++ script.py
@@ -1,28 +1,26 @@
-
-import os, os.path
-
+import os
 import pandas as pd
+import argparse
 
-import argparse 
+parser = argparse.ArgumentParser()
+parser.add_argument("-dir", dest="d",help="Specify the directory for counting sub folders containing images")
+options = parser.parse_args()
+directory = options.d
+try:
 
-parser=argparse.ArgumentParser()
+    lines = [x for x in os.listdir(directory) if os.path.isdir(directory)]
+    dictionary_empty = {}
+    for i in lines:
+        u = directory + i
 
-parser.add_argument("-dir",dest="d",help="Specify the directory for counting subfolders containing images")
+        dictionary_empty[i] = len([x for x in os.listdir(u)
+                                   if x.endswith((".jpg", ".JPG", ".jpeg", ".JPEG", ".png", ".PNG", ".gif", ".GIF"))])
 
-options=parser.parse_args()
+    data_frame = pd.DataFrame.from_dict(dictionary_empty, orient="index")
+    data_frame.to_csv("counts.csv", sep="\t", encoding="utf-8")
 
-Directory = options.d
 
-lines=os.listdir(Directory)
+except OSError:
+    print("The passed path is not a directory")
 
-dictionary_empty={}
 
-for i in lines:
-
-	u=Directory+i
-
-	dictionary_empty[i]=len(os.listdir(u))
-
-data_frame=pd.DataFrame.from_dict(dictionary_empty,orient="index")
-
-data_frame.to_csv("counts.csv", sep="\t", encoding="utf-8")
